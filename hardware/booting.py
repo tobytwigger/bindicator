9ead491f3073c03fd25d9e6c24e89cd68b43d2e1
Toby Twigger <tobytwigger1@gmail.com>
2026-02-08 10:13:10 +0000

    Minor changes to booting script

--- hardware/booting.py
+++ hardware/booting.py
@@ -1,3 +1,10 @@
+#!/home/toby/when-is-bins/python/.venv/bin/python
+import sys
+from pathlib import Path
+
+root_dir = Path(__file__).resolve().parents[1]
+sys.path.append(str(root_dir))
+
 import RPi.GPIO as GPIO
 from hardware.drivers.lcd import Lcd
 import time
